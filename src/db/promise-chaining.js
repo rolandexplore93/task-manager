fa6528d9274553f7ab77b74c15629909cb76d392
rolandexplore93 <roland2rule@gmail.com>
2022-07-19 07:10:03 +0100

    AsyncAwait operations and challenges

--- src/db/promise-chaining.js
+++ src/db/promise-chaining.js
@@ -1,9 +1,20 @@
 require('./mongoose');
 const User = require('../models/User');
 
-User.findByIdAndUpdate('62d6255074eef04f61bb46ae', {age: 29}).then(user => {
-    console.log(user);
-    return User.countDocuments({age: 29})
-}).then(result => {
-    console.log(result)
-}).catch(err => console.log(err))
+// using proming-chaining approach
+// User.findByIdAndUpdate('62d6255074eef04f61bb46ae', {age: 29}).then(user => {
+//     console.log(user);
+//     return User.countDocuments({age: 29})
+// }).then(result => {
+//     console.log(result)
+// }).catch(err => console.log(err))
+
+// using async-await approach
+const updateUserAgeAndCount = async (id, age) => {
+    const updateUserAge = await User.findByIdAndUpdate(id, {age});
+    const count = await User.countDocuments({age})
+    return updateUserAge
+}
+
+updateUserAgeAndCount('62d6255074eef04f61bb46ae', 25).then(result => console.log(result))
+.catch(err => console.log(err))
