84358a4ffbcd156a8f51a9ca6a2763fe8b67b602
rolandexplore93 <roland2rule@gmail.com>
2022-07-20 21:26:09 +0100

    Implement express.router() to handle all routes

---
+++ src/routes/task.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const router = express.Router()
+const Task = require('../models/Task');
+
+// route handlers for tasks
+router.post('/tasks', async (req, res) => {
+    const task = new Task(req.body);
+
+    try {
+        await task.save();
+        res.status(201).send(task);
+
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+router.get('/tasks', async (req, res) => {
+
+    try {
+        const tasks = await Task.find({});
+        res.send(tasks)
+    } catch(e) {
+        res.status(500).send(e)
+    }
+})
+
+router.get('/tasks/:id', async (req, res) => {
+    const _id = req.params.id;
+
+    try {
+        const task = await Task.findById(_id);
+
+        if (!task) {
+            res.status(404).send("Task ID not found!")
+            return
+        }
+        res.send(task)
+    } catch (e) {
+        res.status(500).send("Invalid Task ID")
+    }
+})
+
+
+router.patch("/tasks/:id", async (req, res) => {
+    const _id = req.params.id;
+    const allowUpdates = ["description", "completed"];
+    const updates = Object.keys(req.body);
+    const isValidUpdate = updates.every((update) => allowUpdates.includes(update));
+
+    if (!isValidUpdate) return res.status(400).send(`Error: One or more properties is invalid... \n Accepted property fields are ${allowUpdates.join(', ')}`)
+
+    try {
+        const task = await Task.findByIdAndUpdate(_id, req.body, { new: true, runValidators: true, timestamps: true });
+
+        if (!task) return res.status(404).send("Task ID not found!")
+        res.send(task)
+    } catch(e) {
+        console.log("error:" + e)
+        res.status(400).send(e)}
+})
+
+router.delete("/tasks/:id", async (req, res) => {
+    const _id = req.params.id;
+
+    try {
+        const task = await Task.findByIdAndDelete(_id);
+
+        if (!task) return res.status(404).send("User not found!")
+        res.send(`User with the data below has been deleted from database \n ${task}`)
+    } catch(e) {res.status(500).send(e)}
+})
+
+module.exports = router
---
+++ src/routes/user.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const router = express.Router()
+const User = require('../models/User');
+
+router.post('/users', async (req, res) => {
+    const user = new User(req.body);
+    
+    try {
+        await user.save();
+        res.status(201).send(user);
+    } catch (e) {
+        res.status(400).send(e.message);
+    }
+})
+
+router.get('/users', async (req, res) => {
+
+    try {
+        const users = await User.find({});
+        res.send(users)
+    } catch(e){
+        res.status(500).send(e)
+    }
+})
+
+router.get('/users/:id', async (req, res) => {
+    const _id = req.params.id;
+
+    try {
+        const user = await User.findById(_id)
+        if (!user) {
+            res.status(404).send("User ID not found!")
+            return
+        }
+        res.send(user)
+    } catch(e){
+        res.status(500).send("Invalid User ID")
+    }
+})
+
+router.patch('/users/:id', async (req, res) => {
+    const _id = req.params.id;
+    const allowUpdates = ["name", "age", "email", "password"];
+    const updates = Object.keys(req.body);
+    const isValidUpdate = updates.every((update) => allowUpdates.includes(update));
+
+    if (!isValidUpdate) return res.status(400).send(`Error: One or more properties is invalid... \n Accepted property fields are ${allowUpdates.join(', ')}`)
+
+    try {
+        const user = await User.findByIdAndUpdate(_id, req.body, { new: true, runValidators: true, timestamps: true });
+
+        if (!user) return res.status(404).send("User not found!")
+        res.send(user)
+    } catch(e) {
+        console.log("error:" + e)
+        res.status(400).send(e)}
+})
+
+router.delete("/users/:id", async (req, res) => {
+    const _id = req.params.id;
+
+    try {
+        const user = await User.findByIdAndDelete(_id);
+
+        if (!user) return res.status(404).send("User not found!")
+        res.send(`User with the data below has been deleted from database \n ${user}`)
+    } catch(e) {res.status(500).send(e)}
+})
+
+module.exports = router
