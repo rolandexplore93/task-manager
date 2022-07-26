7353f0a23b2ff31e012b7bf6df0e565f109f85c5
rolandexplore93 <roland2rule@gmail.com>
2022-07-26 09:19:03 +0100

    Set node version in package.json for heroku fix

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const multer = require('multer');
 
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5000;
 
 // app.use((req, res, next) => {
 //     console.log(req.method, req.path)
